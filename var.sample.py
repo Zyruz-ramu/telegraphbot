008d2f476fd5376c6ff1ce938f3d6d48cf5c6551
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-06-25 20:48:55 +0530

    Create var.sample.py

---
+++ var.sample.py
@@ -0,0 +1,7 @@
+# this vars nd values require to run this bot
+
+# get from @BotFather
+BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+# from my.telegram.org
+API_ID = 12345
+API_HASH = "0123456789abcdef0123456789abcdef"
